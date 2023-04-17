a4d45e2854a23e17b381c07c5d8f8da3708a715e
Maxim Klimov <maximklimov@MacBook-Air-Maxim.local>
2023-04-17 22:01:38 +0300

    added form

--- src/init.js
+++ src/init.js
@@ -1,9 +1,13 @@
 // @ts-check
-
-import Example from './Example.js';
+import { object, string } from 'yup';
+// import Example from './Example.js';
 
 export default () => {
-  const element = document.getElementById('point');
-  const obj = new Example(element);
-  obj.init();
+  // const element = document.getElementById('point');
+  // const obj = new Example(element);
+  // obj.init();
+  const urlInput = document.querySelector('#url-input');
+  const validationSchema = object({
+    rssLink: string().url().required(),
+  });
 };
